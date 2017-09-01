07ba8b694e3940531639096d328b11e63375035a
Jonas Almeida <jalmeida@mathbiol.org>
2017-09-01 12:02:46 -0400

    ini

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # gdc
-experimenting with https://gdc.cancer.gov
+Experimenting with Genomic Data Commons [gdc.cancer.gov](https://gdc.cancer.gov). To play with the library in this repository  have a go at the browser’s console at [mathbiol.github.io/gdc](https://mathbiol.github.io/gdc). This tool builds on a similar initiative to operate cBio’s API at [github.com/mathbiol/cbio](https://github.com/mathbiol/cbio).
+___
+
+### get
+the core function that is operated by other GET calls
+
+````javascript
+gdc.get=function(endPoint,query,callbackFunction)
+````
+it also supports JS promises,
+
+````javascript
+gdc.get=function(endPoint,query).then(callbackFunction)
+````
+
+like all other operators described here, for example, these three  expressions are equivalent:
+
+````javascript
+
+gdc.get(’status’,console.log)
+gdc.status(console.log)
+gdc.status().then(console.log)
+
+````
+
+### status
+
+Retrieves status of NCI’s GDC API endpoint.
+
+````javascript
+gdc.status(callbackFunction)
+````
+
+the response will be something like
+
+````JSON
+{
+   "commit": "23e5f2ae47eecadec62b6eca8a117e906b22c946",
+   "data_release": "Data Release 8.0 - August 22, 2017",
+   "status": "OK",
+   "tag": "1.10.0",
+   "version": 1
+}
+````
---
+++ gdc.js
@@ -0,0 +1,90 @@
+console.log('gdc.js loaded at '+Date());
+
+// Genome data commons API info at https://gdc.cancer.gov/developers
+// API docs https://docs.gdc.cancer.gov/API/Users_Guide/Getting_Started/#api-endpoints
+
+gdc=function(url){
+    gdc.url=url||'https://gdc-api.nci.nih.gov'
+    // for older versions url would use the template https://api.gdc.cancer.gov/<version>/legacy/
+    // ini
+    gdc.div = document.getElementById('gdcDiv')
+    //if(gdc.div){
+    //    gdc.div.innerHTML='Cancer Genome Commons loaded :-)'
+    //}
+}
+
+gdc.parms2query=function(pp){ // convert JSON query object into a query string
+    if(typeof(pp)=='object'){
+        var q='?'
+        keys(pp).forEach(function(p){
+            q+=`${p}=${pp[p]}&`
+        })
+        q=q.slice(0,-1) // to remove trailing &
+    }else{ // pp is already a string or may be null, jut pass it on as is
+        q=pp 
+    }
+    return q
+}
+
+gdc.query2parms=function(qq){ // convert query string into JSON query object
+    if(qq[0]=="?"){qq=qq.slice(1)} // in case query string is prefized with '?', remove it
+    pp={}
+    qq.split('&').forEach(function(q){
+        q = q.split('=')
+        pp[q[0]]=q[1]
+        4
+    })
+    return pp
+}
+
+gdc.get=function(endPoint,q,fun){
+    endPoint=endPoint||'status' // get status by default
+    q=q||''
+    q=gdc.parms2query(q)
+    //if(q.length>0){q='/'+q}
+    if(fun){
+        return $.getJSON(gdc.url+'/'+endPoint+q).then(fun)
+    }else{
+        return $.getJSON(gdc.url+'/'+endPoint+q)
+    }
+}
+
+// ENDPOINTS
+// https://docs.gdc.cancer.gov/API/Users_Guide/Getting_Started/#api-endpoints
+// following the list of endpoints listed in that page
+
+gdc.status=function(fun){ // to get the status in the console, one could do gdc.status(console.log)
+    return gdc.get('status',null,fun) // no query parms for status calls
+}
+
+gdc.projectList=function(q,fun){ // list of project
+    // https://docs.gdc.cancer.gov/API/Users_Guide/Search_and_Retrieval/#project-endpoint
+    // for example https://api.gdc.cancer.gov/projects?from=0&size=2&sort=project.project_id:asc&pretty=true
+    // would be gdc.projectList({from:0,size:2,sort:"project.project_id:asc",pretty:true})
+    return gdc.get('projects',q,fun)
+}
+
+gdc.project=function(prj,q,fun){ // list of project
+    // https://docs.gdc.cancer.gov/API/Users_Guide/Search_and_Retrieval/#project-endpoint
+    // for example https://api.gdc.cancer.gov/projects/TARGET-NBL?expand=summary,summary.experimental_strategies,summary.data_categories&pretty=true
+    // would be gdc.project("TARGET-NBL",{expand:"summary,summary.experimental_strategies,summary.data_categories",pretty:true})
+    return gdc.get('projects/'+prj,q,fun)
+}
+
+gdc.caseList=function(q,fun){ // list of project
+    // https://docs.gdc.cancer.gov/API/Users_Guide/Search_and_Retrieval/#cases-endpoint
+    // for example https://api.gdc.cancer.gov/projects?from=0&size=2&sort=project.project_id:asc&pretty=true
+    // would be gdc.projectList({from:0,size:2,sort:"project.project_id:asc",pretty:true})
+    return gdc.get('cases',q,fun)
+}
+
+gdc.case=function(prj,q,fun){ // list of project
+    // https://docs.gdc.cancer.gov/API/Users_Guide/Search_and_Retrieval/#project-endpoint
+    // for example https://api.gdc.cancer.gov/projects/TARGET-NBL?expand=summary,summary.experimental_strategies,summary.data_categories&pretty=true
+    // would be gdc.project("TARGET-NBL",{expand:"summary,summary.experimental_strategies,summary.data_categories",pretty:true})
+    return gdc.get(' cases/'+prj,q,fun)
+}
+
+setTimeout(gdc,0) // change this to check for jQuery availability
+
+//https://gdc-api.nci.nih.gov/projects
